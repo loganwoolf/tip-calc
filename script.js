bbeb7a18905204ef70e62101f4cff06a7e2f9130
Logan Woolf <loganwoolf@gmail.com>
2021-07-14 22:13:33 -0600

    create calc function, add listeners on buttons

--- script.js
+++ script.js
@@ -2,26 +2,39 @@
 
 // define tip rates available on buttons
 const rates = [0.12, 0.15, 0.18, 0.20, 0.25];
+// let currentRate;
 
 // select input elements
 const billField = document.getElementById('price');
 const guestsField = document.getElementById('people')
-const buttons = document.querySelectorAll('.button-label');
+const radioButtons = document.querySelectorAll('input[type=radio]');
 
 //map and format rates to textContent of each button
 rates.map((rate, i) => {
-	buttons[i].textContent = `${rate*100}%`
+	radioButtons[i].labels[0].textContent = `${rate*100}%`
 });
 
 function calculate() {
+	let currentRate;
 	if (billField.value && guestsField.value) {
-		console.log('run calc')
+		currentRate = document
+			.querySelector('input[name=amount]:checked')
+			.labels[0]
+			.textContent
+			.slice(0,-1)
+			/100;
+		let tipAmount = parseInt((billField.value*100) * currentRate / guestsField.value)/100;
+		let totalAmount = parseInt((billField.value*100) * (1 + currentRate) / guestsField.value)/100;
+		console.log(`Tip Amount: ${tipAmount}`)
+		console.log(`Total: ${totalAmount}`)
+		console.log(currentRate);
 	}
 }
 
 
 billField.addEventListener('input', calculate);
 guestsField.addEventListener('input', calculate);
+radioButtons.forEach(button => button.addEventListener('click', calculate));
 
 
 
